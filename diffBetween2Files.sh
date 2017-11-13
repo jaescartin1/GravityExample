68856745d7885e1b224baea3cb21502da8f69196
Jose A. Escartin <ja.escartin@gmail.com>
2017-11-13 23:32:27 +0100

    Added script

--- diffBetween2Files.sh
+++ diffBetween2Files.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-#./execWithCompiler.sh WORK_DIR F77_COMPILER FC_COMPILER CXX_COMPILER
-
-cd $1
-./bootstrap
-
-export F77=$2
-export FC=$3
-export CXX=$4
-./configure "F77=$2" "FC=$3" "CXX=$4"
-
-make check
-
-make clean
-make distclean
-
-cd ../..
-
+#./diffBetween2Files.sh file1 file2 delta
